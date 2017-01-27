170acb692166c8aae83cfb239e7dba4763221eb0
oduvan <a.lyabah@checkio.org>
2017-01-27 21:34:14 +0200

    version

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ requirements = [l.strip() for l in open(join(source_directory, 'requirements.txt
 
 setup(
     name='checkio_task_tester',
-    version='0.0.1',
+    version='0.0.2',
     description='CheckiO common line interface for debugging missions on *.checkio.org',
     author='CheckiO',
     author_email='a.lyabah@checkio.org',
     url='https://github.com/CheckiO/checkio-task-tester',
-    download_url='https://github.com/CheckiO/checkio-task-tester/tarball/0.0.1',
+    download_url='https://github.com/CheckiO/checkio-task-tester/tarball/0.0.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['cio-task-tester = checkio_task_tester.runner:main'],
