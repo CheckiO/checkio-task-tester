a8e0ae5bc3dcf8e2337796cf222b937ff3a2838b
oduvan <a.lyabah@checkio.org>
2017-02-27 17:29:56 +0200

    version

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
+import sys
 from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
+
+if sys.version_info > (3.5):
+    sys.exit('Sorry, The last supported Python version is 3.5')
+
 
 source_directory = dirname(abspath(__file__))
 requirements = [l.strip() for l in open(join(source_directory, 'requirements.txt'))]
 
 setup(
     name='checkio_task_tester',
-    version='0.0.5',
+    version='0.0.6',
     description='CheckiO common line interface for debugging missions on *.checkio.org',
     author='CheckiO',
     author_email='a.lyabah@checkio.org',
     url='https://github.com/CheckiO/checkio-task-tester',
-    download_url='https://github.com/CheckiO/checkio-task-tester/tarball/0.0.5',
+    download_url='https://github.com/CheckiO/checkio-task-tester/tarball/0.0.6',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['cio-task-tester = checkio_task_tester.runner:main'],
